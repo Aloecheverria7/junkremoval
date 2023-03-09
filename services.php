e8917bd682224353fe94ba869981e22a6a65f142
Aloecheverria7 <33984928+Aloecheverria7@users.noreply.github.com>
2023-03-09 04:43:38 -0600

    Cambio en el menu de servicios
    Se alinearon en forma de lista los servicios.

--- services.php
+++ services.php
@@ -1,230 +1,70 @@
-
 <?php include('links.php') ?>
 <?php include('php/secction/header.php') ?>
-    <!--End Main Header --> 
-    
-    
-    <!--Page Title-->
-    <section class="page-title" style="background-image:url(img/resource/remodelacion/p_5.jpg);">
-    	<div class="auto-container">
-        	<h2>Our <span class="theme_color"> <?php echo $SN[0] ?> </span></h2>
-            <div class="text"> <?php echo $SD[0] ?></div>
-        </div>
-    </section>
-    
-    <!--Breadcrumb-->
-    <div class="theme-breadcrumb">
-    	<div class="auto-container">
-            <ol class="breadcrumb" style="margin-bottom: 5px;">
-              <li><a href="index.php">Home</a></li>
-              <li class="active">Services</li>
-            </ol>
-        </div>
+<!--End Main Header -->
+
+
+<!--Page Title-->
+<section class="page-title" style="background-image:url(img/resource/remodelacion/p_5.jpg);">
+    <div class="auto-container">
+        <h2>Our <span class="theme_color"> <?php echo $SN[0] ?> </span></h2>
+        <div class="text"> <?php echo $SD[0] ?></div>
     </div>
+</section>
 
-     <!--Services Section-->
-     <section class="services-style-one no-padd-top">
-    	<div class="auto-container"> 
-            
-        	<div class="row clearfix">
-            	
-                <!--Default Service Column-->
-                <div class="default-service-column col-md-4 col-sm-6 col-xs-12">
-                	<div class="inner-box wow zoomInStable" data-wow-delay="0ms" data-wow-duration="1500ms">
-                    	<div class="inner-most">
-                        	<figure class="image-box"><a href="services.php"><img src="img/resource/d1_resultado.jpg" alt=""></a></figure>
-                            <div class="lower-part">
-                            	<div class="left-curve"></div>
-                                <div class="right-curve"></div>
-                                
-                            	<div class="content">
-                                	<h3> <?php echo $SN[1] ?> </h3>
-                                    <div class="text"> <?php echo $SD[1] ?> </div>
-                                    <div class="more-link"><a href="services.php" class="read-more"></a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <!--Default Service Column-->
-                <div class="default-service-column col-md-4 col-sm-6 col-xs-12">
-                	<div class="inner-box wow zoomInStable" data-wow-delay="500ms" data-wow-duration="1500ms">
-                    	<div class="inner-most">
-                        	<figure class="image-box"><a href="services.php"><img src="img/resource/d2_resultado.jpg" alt=""></a></figure>
-                            <div class="lower-part">
-                            	<div class="left-curve"></div>
-                                <div class="right-curve"></div>
-                                
-                            	<div class="content">
-                                	<h3> <?php echo $SN[2] ?> </h3>
-                                    <div class="text"> <?php echo $SD[2] ?> </div>
-                                    <div class="more-link"><a href="services.php" class="read-more"></a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <!--Default Service Column-->
-                <div class="default-service-column col-md-4 col-sm-6 col-xs-12">
-                	<div class="inner-box wow zoomInStable" data-wow-delay="500ms" data-wow-duration="1500ms">
-                    	<div class="inner-most">
-                        	<figure class="image-box"><a href="services.php"><img src="img/resource/d3_resultado.jpg" alt=""></a></figure>
-                            <div class="lower-part">
-                            	<div class="left-curve"></div>
-                                <div class="right-curve"></div>
-                                
-                            	<div class="content">
-                                	<h3> <?php echo $SN[3] ?> </h3>
-                                    <div class="text"> <?php echo $SD[3] ?> </div>
-                                    <div class="more-link"><a href="services.php" class="read-more"></a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <!--Default Service Column-->
-                <div class="default-service-column col-md-4 col-sm-6 col-xs-12">
-                	<div class="inner-box wow zoomInStable" data-wow-delay="1000ms" data-wow-duration="1500ms">
-                    	<div class="inner-most">
-                        	<figure class="image-box"><a href="services.php"><img src="img/resource/d4_resultado.jpg" alt=""></a></figure>
-                            <div class="lower-part">
-                            	<div class="left-curve"></div>
-                                <div class="right-curve"></div>
-                                
-                            	<div class="content">
-                                    <h3> <?php echo $SN[4] ?> </h3>
-                                    <div class="text"> <?php echo $SD[4] ?> </div>
-                                    <div class="more-link"><a href="services.php" class="read-more"></a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <!--Default Service Column-->
-                <div class="default-service-column col-md-4 col-sm-6 col-xs-12">
-                	<div class="inner-box wow zoomInStable" data-wow-delay="1000ms" data-wow-duration="1500ms">
-                    	<div class="inner-most">
-                        	<figure class="image-box"><a href="services.php"><img src="img/resource/S_7.jpg" alt=""></a></figure>
-                            <div class="lower-part">
-                            	<div class="left-curve"></div>
-                                <div class="right-curve"></div>
-                                
-                            	<div class="content">
-                                    <h3> <?php echo $SN[5] ?> </h3>
-                                    <div class="text"> <?php echo $SD[5] ?> </div>
-                                    <div class="more-link"><a href="services.php" class="read-more"></a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <!--Default Service Column-->
-                <div class="default-service-column col-md-4 col-sm-6 col-xs-12">
-                	<div class="inner-box wow zoomInStable" data-wow-delay="1000ms" data-wow-duration="1500ms">
-                    	<div class="inner-most">
-                        	<figure class="image-box"><a href="services.php"><img src="img/resource/S_3_1.jpg" alt=""></a></figure>
-                            <div class="lower-part">
-                            	<div class="left-curve"></div>
-                                <div class="right-curve"></div>
-                                
-                            	<div class="content">
-                                    <h3> <?php echo $SN[6] ?> </h3>
-                                    <div class="text"> <?php echo $SD[6] ?> </div>
-                                    <div class="more-link"><a href="services.php" class="read-more">    </a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="default-service-column col-md-4 col-sm-6 col-xs-12">
-                	<div class="inner-box wow zoomInStable" data-wow-delay="1000ms" data-wow-duration="1500ms">
-                    	<div class="inner-most">
-                        	<figure class="image-box"><a href="services.php"><img src="img/resource/S_1_2.jpg" alt=""></a></figure>
-                            <div class="lower-part">
-                            	<div class="left-curve"></div>
-                                <div class="right-curve"></div>
-                                
-                            	<div class="content">
-                                    <h3> <?php echo $SN[7] ?> </h3>
-                                    <div class="text"> <?php echo $SD[7] ?> </div>
-                                    <div class="more-link"><a href="services.php" class="read-more">    </a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<!--Breadcrumb-->
+<div class="theme-breadcrumb">
+    <div class="auto-container">
+        <ol class="breadcrumb" style="margin-bottom: 5px;">
+            <li><a href="index.php">Home</a></li>
+            <li class="active">Services</li>
+        </ol>
+    </div>
+</div>
 
+<!--Services Section-->
+<section class="services-style-one no-padd-top">
+    <div class="auto-container">
 
-                <div class="default-service-column col-md-4 col-sm-6 col-xs-12">
-                	<div class="inner-box wow zoomInStable" data-wow-delay="1000ms" data-wow-duration="1500ms">
-                    	<div class="inner-most">
-                        	<figure class="image-box"><a href="services.php"><img src="img/resource/S_2.jpg" alt=""></a></figure>
-                            <div class="lower-part">
-                            	<div class="left-curve"></div>
-                                <div class="right-curve"></div>
-                                
-                            	<div class="content">
-                                    <h3> <?php echo $SN[9] ?> </h3>
-                                    <div class="text"> <?php echo $SD[9] ?> </div>
-                                    <div class="more-link"><a href="services.php" class="read-more">    </a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div class="row clearfix">
 
 
 
-                <div class="default-service-column col-md-4 col-sm-6 col-xs-12">
-                	<div class="inner-box wow zoomInStable" data-wow-delay="1000ms" data-wow-duration="1500ms">
-                    	<div class="inner-most">
-                        	<figure class="image-box"><a href="services.php"><img src="img/resource/S_1.jpg" alt=""></a></figure>
-                            <div class="lower-part">
-                            	<div class="left-curve"></div>
-                                <div class="right-curve"></div>
-                                
-                            	<div class="content">
-                                    <h3> <?php echo $SN[8] ?> </h3>
-                                    <div class="text"> <?php echo $SD[8] ?> </div>
-                                    <div class="more-link"><a href="services.php" class="read-more">    </a></div>
+            <div class="container">
+                <?php for ($i = 1; $i <= 10; $i++) { ?>
+                    <div class="row"><br>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="service-box">
+                                <div class="service-content-holder">
+                                    <h2 class="text"><?php echo $SN[$i]; ?></h2>
+                                    <p><?php echo $SD[$i]; ?></p>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-
-                <div class="default-service-column col-md-4 col-sm-6 col-xs-12">
-                	<div class="inner-box wow zoomInStable" data-wow-delay="1000ms" data-wow-duration="1500ms">
-                    	<div class="inner-most">
-                        	<figure class="image-box"><a href="services.php"><img src="img/resource/S_3.jpg" alt=""></a></figure>
-                            <div class="lower-part">
-                            	<div class="left-curve"></div>
-                                <div class="right-curve"></div>
-                                
-                            	<div class="content">
-                                    <h3> house cleaning </h3>
-                                    <div class="text"> <?php echo $SD[0] ?> </div>
-                                    <div class="more-link"><a href="services.php" class="read-more">    </a></div>
+                        <div class="default-service-column col-md-4 col-sm-6 col-xs-12">
+                            <div class="inner-box wow zoomInStable" data-wow-delay="0ms" data-wow-duration="1500ms">
+                                <div class="inner-most">
+                                    <figure class="image-box"><a href="services.php"><img src="img/resource/Services/<?php echo $i; ?>.jpg" alt=""></a></figure>
+                                    <div class="lower-part">
+                                        <div class="left-curve"></div>
+                                        <div class="right-curve"></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
 
-                
 
+                    </div><br>
+                    <hr>
+                <?php } ?>
 
             </div>
+
+
         </div>
-    </section>
-    
-  
+    </div>
+</section>
+
+
 
 
 
